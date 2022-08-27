c11c7b3d61aac50b2a464006cbcdc574650ff8ed
Ishmael MUHIKIRA <97230697+muhikira@users.noreply.github.com>
2022-08-27 10:27:26 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import'./App.css';
+import ContactMe from './components/ContactMe';
+function App(){
+    return (
+       <div className="App">
+        <header className="App-header">
+            <p>Welcome to my website</p>
+            <h2> Hi I Am Ishmael</h2>
+            <h5> I am SOftware Developer</h5>
+            <hr/>
+            <ContactMe/>
+        </header>
+       </div>
+    )
+}
+export default App;
